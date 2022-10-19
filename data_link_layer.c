4483c05acfeae434fb863f6b4e792386d9f95368
antoniosantos1602 <up201907156@up.pt>
2022-10-19 16:46:43 +0100

    llopen

--- data_link_layer.c
+++ data_link_layer.c
@@ -70,7 +70,59 @@ int llwrite(int fd, char * buffer, int length){
 int llread(int fd, char * buffer){
 
 }
+/*
+TRANSMITTER:
+Sends command DISC
+Receives DISC command 
+Sends UA message 
 
-int llclose(int fd){
+RECEIVER:
+Sends command DISC 
+Receives UA message 
+
+em caso de sucesso retorna 0
+insucesso -1
+*/
+
+int llclose(int fd, unsigned char , unsigned char ){
+    if (sender == TRANSMITTER){
+        send_commands()
+    }
+    if (sender == RECEIVER){
+
+    }
+    close(fd);
+}
+
+/* Enviar mensagem */
+void send_msg(int fd, ){
+    
+    unsigned char buf[SET_UA_BUF_SIZE];   
+    buf[0] = FLAG;
+    buf[1] = A_EMI;
+    buf[2] = C_SET;
+    buf[3] = BCC_1;
+    buf[4] = FLAG;
+
+    write(fd, buf, SET_UA_BUF_SIZE); 
+}
+
+/* 
+
+*/
+
+void send_commands(int sender, , ){
+    unsigned char a;
+    if (sender == TRANSMITTER){
+        a = A_EMI;
+    }
+    else if (sender == RECEIVER){
+        a = A_REC;
+    }
+    else {
+        return -1;
+    }
+
+    int res = send_msg(fd,a,C_SET);
 
 }
